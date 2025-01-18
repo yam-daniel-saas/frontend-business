3268b7989c467fed28a6cc54f49b35363a35932e
daniel <daniel15898@gmail.com>
2025-01-18 16:26:53 +0200

    add auth to layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout() {
     <KeyboardProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       </Stack>
     </KeyboardProvider>
   );
