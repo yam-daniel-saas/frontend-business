7fecaa416625d809e88a54ffa56cc0afc6e19cf3
daniel <daniel15898@gmail.com>
2025-01-18 16:27:14 +0200

    manualy button to nav to auth

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Button from '@/components/Button';
+import { router } from 'expo-router';
 
 const Index = () => {
+  const handleButtonPress = () => {
+    router.replace('/(auth)/sign-in');
+  };
   return (
     <SafeAreaView>
       <View>
+        <Button onPress={handleButtonPress} className="self-start px-2 py-1">
+          Navigate to Auth
+        </Button>
         <Text>hello world</Text>
       </View>
     </SafeAreaView>
